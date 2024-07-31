188e26bb59e43ea7b3964f4e1fdeecdc803ff599
ntwalii <ntwaliaubin@gmail.com>
2024-07-31 09:31:46 +0200

    Branch resolutions

--- src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Input(props){
-    return (
-        <input type="text" className="px-5 py-2 w-1/2 rounded-md" placeholder={props.placeholder}/>
-    )
-}
